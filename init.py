6d6c4a30f59c4f4c22207cef0cc8190f92df8b99
jcarriel <jcarrielroca98@gmail.com>
2023-10-06 16:03:42 -0500

    segundo commit

---
+++ init.py
@@ -0,0 +1,9 @@
+frase="Estoy programando"
+print(frase[0])
+i=6
+print(frase[i])
+
+#El operador [ ] (corchetes) permite obtener un carácter a partir de un string. La posición del carácter se 
+indica entre los corchetes, ya sea ingresando directamente el número, con una variable que contenga un número 
+o con una operación que de como resultado un número. Siempre, el primer carácter de un string estará ubicado en 
+la posición 0.

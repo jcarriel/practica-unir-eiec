3585ba5880d345fed5b72e79d78fe00392dd8b77
gabrielvela <armando.vela@epn.edu.ec>
2023-10-11 15:25:18 -0500

    Implementación del listado de integrantes del equipo

--- init.py
+++ init.py
@@ -3,7 +3,12 @@ print(frase[0])
 i=6
 print(frase[i])
 
-#El operador [ ] (corchetes) permite obtener un carácter a partir de un string. La posición del carácter se 
-indica entre los corchetes, ya sea ingresando directamente el número, con una variable que contenga un número 
-o con una operación que de como resultado un número. Siempre, el primer carácter de un string estará ubicado en 
-la posición 0.
+#El operador [ ] (corchetes) permite obtener un carácter a partir de un string. La posición del carácter se indica entre los corchetes, ya sea ingresando directamente el número, con una variable que contenga un número o con una operación que de como resultado un número. Siempre, el primer carácter de un string estará ubicado en la posición 0.
+
+print("***Listado integrantes del equipo***");
+
+listado = ["Jean","Vanessa","Jonathan","Gabriel"]
+for i in listado:
+    print(i);
+else:
+    print("fin del listado");

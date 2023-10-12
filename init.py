5ab9c6e0fd896726b81eaaa98eaeff35084c91db
vane1995 <vaneguala95@gmail.com>
2023-10-11 22:35:45 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,9 +1,6 @@
-frase="Estoy programando"
-print(frase[0])
-i=6
-print(frase[i])
+a=0
+a=a+1
+a+=1
+print(a)
 
-#El operador [ ] (corchetes) permite obtener un carácter a partir de un string. La posición del carácter se 
-indica entre los corchetes, ya sea ingresando directamente el número, con una variable que contenga un número 
-o con una operación que de como resultado un número. Siempre, el primer carácter de un string estará ubicado en 
-la posición 0.
+#Las dos instrucciones de arriba son equivalentes (cualquiera de ellas hace lo mismo: sumar 1 al valor almacenado en la variable a y reemplazar el valor anterior de a con el nuevo resultado).
